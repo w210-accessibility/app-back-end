f54d959fbcf62015f8eb3812ebf94cbeea612759
Emily Rapport <emilyrapport16@gmail.com>
2020-02-23 16:37:36 -0600

    followed this tutorial for setting up db: https://blog.memcachier.com/2018/11/13/flask-elastic-beanstalk-and-memcache/

--- application.py
+++ application.py
@@ -1,35 +1,6 @@
-from flask import Flask
-from flask import request, jsonify
-from ClientPrediction import ClientPrediction
-from flask_cors import CORS
+from mvp_endpoint import create_app, db
 
-# EB looks for an 'application' callable by default.
-application = Flask(__name__)
-CORS(application)
-
-# add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: '<h1>Hi</h1>'))
-
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-@application.route('/api/predictions/', methods=['GET'])
-def get_data_for_bounding_box():
-    lat1 = try_parse_int(request.args.get('lat1'))
-    long1 = try_parse_int(request.args.get('long1'))
-    lat2 = try_parse_int(request.args.get('lat2'))
-    long2 = try_parse_int(request.args.get('long2'))
-
-    # PLACEHOLDER making a couple generic objects to pass back
-    return_list = [ClientPrediction(lat1, long1, 0), ClientPrediction(lat2,long2,2)]
-
-    return jsonify({'preds': [pred.to_dict() for pred in return_list]})
-
-def try_parse_int(inp):
-    try:
-        x = int(inp)
-    except:
-        x = 0
-    return x
+application = create_app()
 
 # run the app.
 if __name__ == "__main__":
---
+++ mvp_endpoint/mvp_endpoint.py
@@ -0,0 +1,35 @@
+from flask import (
+    Blueprint, flash, redirect, render_template, request, url_for
+)
+from mvp_endpoint.ClientPrediction import *
+from mvp_endpoint import db
+from mvp_endpoint.models import Task
+from flask import jsonify
+
+bp = Blueprint('mvp_endpoint', __name__)
+
+# add a rule for the index page.
+@bp.route('/', methods=['Get'])
+def index():
+    return '<h1>Hi</h1>'
+
+# add a rule when the page is accessed with a name appended to the site
+# URL.
+@bp.route('/api/predictions/', methods=['GET'])
+def get_data_for_bounding_box():
+    lat1 = try_parse_int(request.args.get('lat1'))
+    long1 = try_parse_int(request.args.get('long1'))
+    lat2 = try_parse_int(request.args.get('lat2'))
+    long2 = try_parse_int(request.args.get('long2'))
+
+    # PLACEHOLDER making a couple generic objects to pass back
+    return_list = [ClientPrediction(lat1, long1, 0), ClientPrediction(lat2,long2,2)]
+
+    return jsonify({'preds': [pred.to_dict() for pred in return_list]})
+
+def try_parse_int(inp):
+    try:
+        x = int(inp)
+    except:
+        x = 0
+    return x
