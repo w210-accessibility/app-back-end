1e1948aa3002867534c6dcd66231917a62d7d6cd
Emily Rapport <emilyrapport16@gmail.com>
2020-02-29 09:59:24 -0600

    added point feature table

--- mvp_endpoint/models.py
+++ mvp_endpoint/models.py
@@ -1,8 +1,12 @@
 from mvp_endpoint import db
 
-class Task(db.Model):
+# TODO figure out how to map label ints to actual labels
+# switch to anume type? or make a type table?
+class PointFeature(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Text(), nullable=False)
-
-    def __repr__(self):
-        return '<Task: {}>'.format(self.name)
+    panoId = db.Column(db.Text(), nullable=False)
+    lat = db.Column(db.Float, nullable=False)
+    long = db.Column(db.Float, nullable=False)
+    roadId = db.Column(db.Integer, nullable=False)
+    label = db.Column(db.Integer, nullable=False)
+    source = db.Column(db.Text(), nullable=False)
