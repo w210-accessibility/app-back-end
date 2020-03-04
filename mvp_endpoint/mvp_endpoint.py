7fb7f8084e50d223d0520edd3bc1ef990dae9211
Emily Rapport <emilyrapport16@gmail.com>
2020-03-03 22:29:17 -0600

    updated endpont to actually return the geojson

--- mvp_endpoint/mvp_endpoint.py
+++ mvp_endpoint/mvp_endpoint.py
@@ -3,7 +3,7 @@ from flask import (
 )
 from mvp_endpoint.ClientPrediction import *
 from mvp_endpoint import db
-from mvp_endpoint.models import Pano, PanoFeature, SidewalkSegment2
+from mvp_endpoint.models import Pano, PanoFeature, SidewalkSegment2, SidewalkSegment3, SegmentToPano2
 from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 
@@ -19,28 +19,26 @@ def index():
 @bp.route('/api/predictions/', methods=['GET'])
 def get_data_for_bounding_box():
     # for mapbox, bounding boxes go (SW, NE)
-    sw_lat = try_parse_int(request.args.get('lat1'))
-    sw_long = try_parse_int(request.args.get('long1'))
-    ne_lat = try_parse_int(request.args.get('lat2'))
-    ne_long = try_parse_int(request.args.get('long2'))
+    sw_lat = try_parse_float(request.args.get('lat1'))
+    sw_long = try_parse_float(request.args.get('long1'))
+    ne_lat = try_parse_float(request.args.get('lat2'))
+    ne_long = try_parse_float(request.args.get('long2'))
 
     results = SidewalkSegment2.query.filter(SidewalkSegment2.startLat >= sw_lat) \
                                     .filter(SidewalkSegment2.startLat <= ne_lat) \
-                                    .filter(SidewalkSegment2.startLong <= sw_long) \
-                                    .filter(SidewalkSegment2.startLong >= ne_long).all()
+                                    .filter(SidewalkSegment2.startLong >= sw_long) \
+                                    .filter(SidewalkSegment2.startLong <= ne_long).all()
 
-    # this is just a hacky little test
-    # next I need to figure out if I need to combine all these into one featureCollection or not
-    if len(results) == 0:
-        result = {}
-    else:
-        result = results[0].geoJson
-    return jsonify({"number of results": len(results),
-                    "first geoJson": result})
+    response_geojson = {"type": "FeatureCollection"}
+    feature_list = []
+    for result in results:
+        feature_list.append(result.geoJson["features"])
+    response_geojson["features"] = feature_list
+    return jsonify(response_geojson)
 
-def try_parse_int(inp):
+def try_parse_float(inp):
     try:
-        x = int(inp)
+        x = float(inp)
     except:
         x = 0
     return x
