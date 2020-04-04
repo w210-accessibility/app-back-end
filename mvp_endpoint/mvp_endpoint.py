1bb20fc29b8c2441d6a8acd1309834d5eeeae809
Emily Rapport <emilyrapport16@gmail.com>
2020-04-04 12:36:17 -0500

    added get endpoint for receiving in situ feedback, plus some leftover related to reloading data into sidewalkSegment4

--- mvp_endpoint/mvp_endpoint.py
+++ mvp_endpoint/mvp_endpoint.py
@@ -3,7 +3,7 @@ from flask import (
 )
 from mvp_endpoint.ClientPrediction import *
 from mvp_endpoint import db
-from mvp_endpoint.models import Pano, PanoFeature, SidewalkSegment2, SidewalkSegment3, SegmentToPano2, SegmentToPano3, InSituFeedback
+from mvp_endpoint.models import Pano, PanoFeature, SidewalkSegment2, SidewalkSegment3, SidewalkSegment4, SegmentToPano2, SegmentToPano3, InSituFeedback
 from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 import json
@@ -26,34 +26,27 @@ def get_data_for_bounding_box():
     ne_lat = try_parse_float(request.args.get('lat2'))
     ne_long = try_parse_float(request.args.get('long2'))
 
-    passable_results = SidewalkSegment3.query.filter(SidewalkSegment3.startLat >= sw_lat) \
-                                             .filter(SidewalkSegment3.startLat <= ne_lat) \
-                                             .filter(SidewalkSegment3.startLong >= sw_long) \
-                                             .filter(SidewalkSegment3.startLong <= ne_long) \
-                                             .filter(SidewalkSegment3.roadGrade <= .05) \
+    passable_results = SidewalkSegment4.query.filter(SidewalkSegment4.startLat >= sw_lat) \
+                                             .filter(SidewalkSegment4.startLat <= ne_lat) \
+                                             .filter(SidewalkSegment4.startLong >= sw_long) \
+                                             .filter(SidewalkSegment4.startLong <= ne_long) \
+                                             .filter(SidewalkSegment4.roadGrade <= .05) \
                                              .all()
 
-    part_passable_results = SidewalkSegment3.query.filter(SidewalkSegment3.startLat >= sw_lat) \
-                                                  .filter(SidewalkSegment3.startLat <= ne_lat) \
-                                                  .filter(SidewalkSegment3.startLong >= sw_long) \
-                                                  .filter(SidewalkSegment3.startLong <= ne_long) \
-                                                  .filter(SidewalkSegment3.roadGrade >= .05) \
-                                                  .filter(SidewalkSegment3.roadGrade <= .1) \
+    part_passable_results = SidewalkSegment4.query.filter(SidewalkSegment4.startLat >= sw_lat) \
+                                                  .filter(SidewalkSegment4.startLat <= ne_lat) \
+                                                  .filter(SidewalkSegment4.startLong >= sw_long) \
+                                                  .filter(SidewalkSegment4.startLong <= ne_long) \
+                                                  .filter(SidewalkSegment4.roadGrade >= .05) \
+                                                  .filter(SidewalkSegment4.roadGrade <= .1) \
                                                   .all()
 
-    impassable_results = SidewalkSegment3.query.filter(SidewalkSegment3.startLat >= sw_lat) \
-                                                .filter(SidewalkSegment3.startLat <= ne_lat) \
-                                                .filter(SidewalkSegment3.startLong >= sw_long) \
-                                                .filter(SidewalkSegment3.startLong <= ne_long) \
-                                                .filter(SidewalkSegment3.roadGrade >= .1) \
+    impassable_results = SidewalkSegment4.query.filter(SidewalkSegment4.startLat >= sw_lat) \
+                                                .filter(SidewalkSegment4.startLat <= ne_lat) \
+                                                .filter(SidewalkSegment4.startLong >= sw_long) \
+                                                .filter(SidewalkSegment4.startLong <= ne_long) \
+                                                .filter(SidewalkSegment4.roadGrade >= .1) \
                                                 .all()
-
-    # what we need to do:
-    # get all features that are MCR (id=1)
-    # connect them to their sidewalk segments through bridge table
-    # based on wehther sidewalk segment says start or end, get that point
-    #no_curb_ramp_marker = PanoFeature.query.filter(PanoFeature.label=1) \
-    #                                       .join(SegmentToPano3.)
 
     response_geojson = {}
     passable_sidewalks = [res.geoJson for res in passable_results]
@@ -65,20 +58,20 @@ def get_data_for_bounding_box():
     response_geojson["missing_sidewalk"] = part_passable_sidewalks
     response_geojson["sidewalk_issues"] = impassable_sidewalks
 
-    # num_passable_chunks = math.ceil(len(passable_sidewalks) / 5000)
-    # for i in range(1, num_passable_chunks):
-    #     end_idx = i*5000
-    #     if end_idx > len(passable_sidewalks):
-    #         end_idx = len(num_passable_chunks) + 1
-    #     tileset_json = {"passable_sidewalks": passable_sidewalks[(i-1)*5000:end_idx]}
-    #     with open('passable_data_{}.json'.format(i), 'w') as outfile:
-    #         json.dump(tileset_json, outfile)
-    # with open('part_passable_data.json', 'w') as outfile:
-    #     tileset_json = {"part_passable_sidewalks": part_passable_sidewalks}
-    #     json.dump(tileset_json, outfile)
-    # with open('impassable_data.json', 'w') as outfile:
-    #     tileset_json = {"impassable_sidewalks": impassable_sidewalks}
-    #     json.dump(tileset_json, outfile)
+    num_passable_chunks = math.ceil(len(passable_sidewalks) / 5000)
+    for i in range(1, num_passable_chunks):
+        end_idx = i*5000
+        if end_idx > len(passable_sidewalks):
+            end_idx = len(num_passable_chunks) + 1
+        tileset_json = {"passable_sidewalks": passable_sidewalks[(i-1)*5000:end_idx]}
+        with open('mar29_passable_data_{}.json'.format(i), 'w') as outfile:
+            json.dump(tileset_json, outfile)
+    with open('mar29_part_passable_data.json', 'w') as outfile:
+        tileset_json = {"part_passable_sidewalks": part_passable_sidewalks}
+        json.dump(tileset_json, outfile)
+    with open('mar29_impassable_data.json', 'w') as outfile:
+        tileset_json = {"impassable_sidewalks": impassable_sidewalks}
+        json.dump(tileset_json, outfile)
     return jsonify(response_geojson)
 
 def try_parse_float(inp):
@@ -100,3 +93,13 @@ def add_in_situ_feedback():
     except:
         pass
     return {"db_success_indicator": status}
+
+@bp.route('/api/getInSitu/', methods=['GET'])
+def get_in_situ_feedback():
+    response = {}
+    # we can distinguish this query more if we want to have different symbols
+    # for different things
+    all_feedback = InSituFeedback.query.all()
+    result_list = [{'location': [res.long, res.lat], 'label': res.label} for res in all_feedback]
+    response['in_situ_results'] = result_list
+    return jsonify(response)
